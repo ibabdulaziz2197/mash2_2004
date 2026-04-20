d6aeae73fcc894af329922da86750a37aefa191f
Abdulaziz <ibabdulaziz2197@gmail.com>
2026-04-20 15:52:37 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# # 2
+# class Car:
+#     def __init__(self, model, color):
+#         self.model = model
+#         self.color  = color
+#
+#     def intoduce(self):
+#         print(f"Bu {self.model} yoshm {self.color}")
+#
+#
+# s1 = Car(Bu Cobalt, rangi oq")
+#
+# s1.intoduce()
